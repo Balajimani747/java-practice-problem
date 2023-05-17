40fe16b4669fe0c483ec7224aec53a20627912d7
John Wick <balajimani747@gmail.com>
2023-05-17 11:25:52 +0530

    fourth commit

---
+++ src/java_practice_problems/Addtonumber.java
@@ -0,0 +1,8 @@
+package java_practice_problems;
+
+public class Addtonumber {
+	public static void main(String[] args) {
+		System.out.print(74 + 36);
+	}
+
+}
---
+++ src/java_practice_problems/Dividtwonumber.java
@@ -0,0 +1,8 @@
+package java_practice_problems;
+
+public class Dividtwonumber {
+	public static void main(String[] args) {
+		System.out.print(50/3);
+	}
+
+}
