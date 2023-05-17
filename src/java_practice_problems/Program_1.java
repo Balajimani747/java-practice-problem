58cbc1b5817f91d36f9708bfcf44a1e034ef0d7b
John Wick <balajimani747@gmail.com>
2023-05-17 11:14:01 +0530

    third commit

--- src/java_practice_problems/Program_1.java
+++ src/java_practice_problems/Program_1.java
@@ -2,7 +2,8 @@ package java_practice_problems;
 
 public class Program_1 {
 	public static void main(String[] args) {
-		System.out.print("Hellow");
+		System.out.print("Hellow ");
+		System.out.print("Balaji");
 	}
 
 }
